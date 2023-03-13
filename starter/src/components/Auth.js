de668576c2636d1b4d5e7e503d8363496e6c6404
shehrbanoNiazi <120422470+shehrbanoNiazi@users.noreply.github.com>
2023-03-13 14:37:48 -0400

    Update Auth.js

--- starter/src/components/Auth.js
+++ starter/src/components/Auth.js
@@ -22,7 +22,7 @@ const Auth = () => {
             password
         }
 
-        const url = 'https://socialmtn.devmountain.com'
+        const url = 'http://localhost:3000'
 
         axios.post(register ? `${url}/register` : `${url}/login`, body)
             .then((res) => {
